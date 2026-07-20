3c7e4fa2d0e5a14f87cc063862031201bd8e5eb2
DrakeLeo <wub8571@gmail.com>
2026-07-20 19:14:01 +0800

    判断水仙花数

--- 01.py
+++ 01.py
@@ -1,2 +1,13 @@
 n = int(input("请输入一个整数："))
 sum = 0
+n0 = n
+deg = len(str(n))
+while n > 0:
+    i = n % 10
+    sum += i **deg
+n = n // 10
+
+if sum == n0:
+    print(f"{n0} 是水仙花数")
+else:
+    print(f"{n0} 不是水仙花数")
